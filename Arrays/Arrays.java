2f567ec886115a6bda541f390c8173458ac1a2f1
Vivek Chand Nirala <121417084+vivekchandnirala@users.noreply.github.com>
2024-01-08 01:48:01 +0530

    Add files via upload

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -1,25 +1,25 @@
-public class Array {
+import java.util.Scanner;
+
+//Question : Take an array of names as input from the user and print them on the screen.
+public class Arrays {
     public static void main(String[] args){
 
-/*
-   1st Method of Declaring the Arrays
+        //Taking input From the User
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Size of Arrays : ");
+        int size = sc.nextInt();
 
-       //We are Declaring Array and the size of Array
-        int[] marks = new int[3];
-        // Now we are Declaring the Value
-        marks[0]=97;
-        marks[1]=87;
-        marks[2]=99;
-*/
+        //Declaring Arrays
+        String name[] = new String[size];
 
- // 2nd Method of Declaring the Arrays
-
- int[] marks = {84,96,85,74,99};
-
-        // Now we Print the Values Of array through For Loop
-        for (int i=0;i<5;i++){
-            System.out.println("Marks are "+marks[i]);
+        //Taking No. of Times for names
+        for (int i=0; i<size;i++){
+            System.out.print("Enter the Name : ");
+            name[i]=sc.next();
         }
 
+        //Display the Arrays
+        for (int i=0; i<size; i++){
+            System.out.println("name " + (i+1) +" is : " + name[i]);        }
     }
 }
